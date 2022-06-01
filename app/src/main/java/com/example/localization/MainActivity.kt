8f3c69db1b2f8e6a9edefb5df2d84d2700231b95
JudiciousX <1119833684@qq.com>
2022-06-01 18:56:14 +0800

    Kotlin first demo About changing languages according to system language transformations

---
+++ app/src/main/java/com/example/localization/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.localization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView2)
+        button.setOnClickListener {
+            textView.text = getText(R.string.msg)
+        }
+    }
+}
